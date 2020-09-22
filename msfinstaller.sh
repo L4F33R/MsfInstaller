836572be7aa516ce19602a3f181d919133bf8861
MUHAMED LAFEER <71687916+L4F33R@users.noreply.github.com>
2020-09-22 11:58:10 +0530

    Update msfinstaller.sh

--- msfinstaller.sh
+++ msfinstaller.sh
@@ -7,7 +7,14 @@ gunzip metasploit_5.0.56_android5_all.deb.gz
 dpkg -i metasploit_5.0.56_android5_all.deb
 apt install -f -y
 clear
+echo ""
+echo ""
+echo ""
+echo ""
 echo "You Can Now Run The Metasploit Framework As msfconsole and msfvenom etc.."
+echo ""
 echo "Subscribe My Yt : PY-Linux   or simply search(#pylinux)"
+echo ""
 echo "my blog https://py-linux.blogspot.com/"
+echo ""
 echo "Lets Start Penetrate Testing With MSF *_*"
