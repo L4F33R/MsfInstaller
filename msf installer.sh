b8a93151fbba49c7a14a14ca1c6b6a1903b0ab90
Lafeer007 <71687916+Lafeer007@users.noreply.github.com>
2020-09-22 11:26:32 +0530

    msfinstaller.sh

--- msf installer.sh
+++ msf installer.sh
@@ -6,3 +6,8 @@ curl -LO https://github.com/termux/termux-packages/metasploit_5.0.56_android5_al
 gunzip metasploit_5.0.56_android5_all.deb.gz
 dpkg -i metasploit_5.0.56_android5_all.deb
 apt install -f -y
+clear
+echo "You Can Now Run The Metasploit Framework As msfconsole and msfvenom etc.."
+echo "Subscribe My Yt : PY-Linux   or simply search(#pylinux)"
+echo "my blog https://py-linux.blogspot.com/"
+echo "Lets Start Penetrate Testing With MSF *_*"
