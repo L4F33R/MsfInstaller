a07fda8ce1bde205a342095b5e1f4fc0852ef459
MUHAMED LAFEER <71687916+L4F33R@users.noreply.github.com>
2020-09-27 22:22:01 +0530

    Update msfinstaller.sh

--- msfinstaller.sh
+++ msfinstaller.sh
@@ -1,5 +1,6 @@
 pkg install git -y
 pkg install curl -y
+pkg install ruby -y
 pkg install gem -y
 gem install bundler:1.17.3
 curl -LO https://github.com/termux/termux-packages/metasploit_5.0.56_android5_all.deb.gz
